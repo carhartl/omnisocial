e2cd833896cf240871ad45ae4017cb50babc9696
Klaus Hartl <klaus.hartl@gmail.com>
2010-12-30 23:50:14 +0100

    Making Omnisocial::User an abstract class, so that User can act as target for polymorphic belongs_to associations (type columns do store 'User' instead of 'Omnisocial::User') - for instance to play nice with acts_as_follower plugin

--- app/models/omnisocial/user.rb
+++ app/models/omnisocial/user.rb
@@ -1,8 +1,13 @@
 module Omnisocial
   class User < ActiveRecord::Base
+    # So that type columns for polymorphic associations targeting
+    # User < Omnisocial::User do have the correct value 'User'
+    # instead of 'Omnisocial::User'...
+    self.abstract_class = true
+
     has_one :login_account, :class_name => 'Omnisocial::LoginAccount', :dependent => :destroy
     delegate :login, :name, :picture_url, :account_url, :access_token, :to => :login_account
-  
+
     def to_param
       if !self.login.include?('profile.php?')
         "#{self.id}-#{self.login.gsub('.', '-')}"
@@ -10,11 +15,11 @@ module Omnisocial
         self.id.to_s
       end
     end
-  
+
     def from_twitter?
       login_account.kind_of? TwitterAccount
     end
-  
+
     def from_facebook?
       login_account.kind_of? FacebookAccount
     end
@@ -22,7 +27,7 @@ module Omnisocial
     def remember
       update_attributes(:remember_token => ::BCrypt::Password.create("#{Time.now}-#{self.login_account.type}-#{self.login}")) unless new_record?
     end
-  
+
     def forget
       update_attributes(:remember_token => nil) unless new_record?
     end
